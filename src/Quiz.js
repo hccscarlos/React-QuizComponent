c3471dc5a78b8e75a14a67c3b670f8a4895a311a
Carlos Delerme <39924235+hccscarlos@users.noreply.github.com>
2021-10-30 14:44:41 -0400

    Step 5
    Creating a QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd'
 
 let quizData = require('./quiz_data.json');
 
@@ -10,10 +11,25 @@ class Quiz extends Component {
     }
 
     render(){
+        // const isQuizEnd = (this.state.quiz_position === quizData.quiz_questions.length);
+        const isQuizEnd = true;
+        console.log("render()", {isQuizEnd, quiz_position:this.state.quiz_position, quiz_questions:quizData.quiz_questions.length});
         return (
-            <QuizQuestion 
-                quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
-            />
+            <main>
+                <section>
+                <p>{quizData.quiz_questions[0].instruction_text}</p>
+                </section>
+                {isQuizEnd ?
+                <QuizEnd></QuizEnd>
+                :
+                <section className="buttons">
+                <ul>
+                    <QuizQuestion 
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                    />
+                </ul>
+                </section>}
+            </main>
         )
     }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,18 +9,9 @@ class QuizQuestion extends Component {
 
     render(){
         return (
-            <main>
-                <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
-                </section>
-                <section className="buttons">
-                <ul>
-                    <QuizQuestionButton 
-                        button_text={this.props.quiz_question.answer_options[0]}
-                    />
-                </ul>
-                </section>
-            </main>
+            <QuizQuestionButton 
+                button_text={this.props.quiz_question.answer_options[0]}
+            />
         )
     }
 
